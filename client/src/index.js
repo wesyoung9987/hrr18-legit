78f6cc008d13b9d314d8d9b2e05846bcd23cf3ab
Jonathon Fritz <jfritz50@gmail.com>
2016-09-08 14:07:20 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,28 +7,22 @@ import App from './components/App.jsx';
 
 // Router
 import {Router, browserHistory } from 'react-router';
-                // browerHistory pays attention to when there's a change to the url pathname
+// browerHistory pays attention to when there's a change to the url pathname
 import routes from './routes'
 
 // Redux
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-<<<<<<< HEAD
 import ReduxPromise from 'redux-promise';
-=======
->>>>>>> 1a6afcbf1e597987ebf76590063ac46ce08668e5
-
 import reducers from './reducers/index.js';
 
 // where our app will be rendered within index.html
 let rootElement = document.getElementById('app');
 
 // there exists only one store in redux and it is what contains our apps state
-<<<<<<< HEAD
+
 let createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-=======
 let createStoreWithMiddleware = applyMiddleware()(createStore);
->>>>>>> 1a6afcbf1e597987ebf76590063ac46ce08668e5
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
