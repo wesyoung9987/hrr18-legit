e70460546d83334b747ea579b241a749e0c274d3
formatted <ronivegh@gmail.com>
2016-09-17 12:01:07 -0600

    changed back to original

--- server/database/database_config.js
+++ server/database/database_config.js
@@ -3,13 +3,10 @@ var Sequelize = require('sequelize');
 if (process.env.NODE_ENV !== 'production') {
 
   // config.js is ignored by Git
-  // just for heroku
   var config = require('../../config.js');
-
 }
 
 var connection = process.env.DATABASE_URL || config.connection;
-// var connection = process.env.DATABASE_URL;
 
 var sequelize = new Sequelize(connection,
   {
